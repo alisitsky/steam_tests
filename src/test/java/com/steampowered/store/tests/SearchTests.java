8fd53b829f48cd43a69e9314c65d0783a676ea93
alisitsky <alisitsky@gmail.com>
2024-04-16 17:08:44 +0200

    SearchTests added

--- src/test/java/com/steampowered/store/tests/SearchTests.java
+++ src/test/java/com/steampowered/store/tests/SearchTests.java
@@ -1,4 +1,33 @@
 package com.steampowered.store.tests;
 
-public class SearchTests {
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.ARROW_DOWN;
+import static org.openqa.selenium.Keys.ENTER;
+
+public class SearchTests extends TestBase {
+
+    @Test
+    public void selectFromSearchSuggestByClickTest() {
+        open(baseUrl);
+        $("input#store_nav_search_term").setValue("Disco Elysium");
+        $("div#search_suggestion_contents").shouldBe(visible);
+        $("a.match_category_top").click();
+        $("div#appHubAppName").shouldHave(text("Disco Elysium"));
+    }
+
+    @Test
+    public void selectFromSearchSuggestWithKeyboardTest() {
+        open(baseUrl);
+        $("input#store_nav_search_term").setValue("Disco Elysium");
+        $("div#search_suggestion_contents").shouldBe(visible);
+        $("input#store_nav_search_term").sendKeys(ARROW_DOWN, ENTER);
+        $("div#appHubAppName").shouldHave(text("Disco Elysium"));
+    }
+
 }
